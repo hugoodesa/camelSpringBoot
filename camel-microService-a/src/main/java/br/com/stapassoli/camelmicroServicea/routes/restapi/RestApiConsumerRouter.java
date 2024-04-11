e0e0ad9082e4b2928121d5d1635b16cf3c2436b1
Hugo Luiz Stapassoli de sá <63262596+hugoodesa@users.noreply.github.com>
2024-04-10 21:54:51 -0300

    Update RestApiConsumerRouter.java

--- camel-microService-a/src/main/java/br/com/stapassoli/camelmicroServicea/routes/restapi/RestApiConsumerRouter.java
+++ camel-microService-a/src/main/java/br/com/stapassoli/camelmicroServicea/routes/restapi/RestApiConsumerRouter.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Component;
 //@Component
 public class RestApiConsumerRouter extends RouteBuilder {
 
+    //choose one of the two methods
 
     //http://localhost:8080/currency?from=br&to=us
+    //MODE 1
     @Override
     public void configure() throws Exception {
 
@@ -17,4 +19,19 @@ public class RestApiConsumerRouter extends RouteBuilder {
 
     }
 
+    @Override
+    //MODE 2
+    public void configure() throws Exception {
+
+        restConfiguration()
+            .host("https://api.github.com/users")
+            .component("servlet")
+            .bindingMode(RestBindingMode.auto);;
+
+        from("timer:githubTimer?period=60000")
+            .removeHeaders("CamelHttp*")
+            .to("rest:get:/hugoodesa/repos")
+            .log("Response from GitHub API: ${body}");
+    }
+    
 }
